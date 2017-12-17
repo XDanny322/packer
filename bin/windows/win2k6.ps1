31bfb898b2ab8c6a2a6e57f3d08d996c2cd8ce86
Danny Lai <dlai@portware.com>
2017-12-17 01:36:38 -0500

    Added win2k6, running in windows

--- bin/windows/win2k6.ps1
+++ bin/windows/win2k6.ps1
@@ -18,9 +18,13 @@ Get-ChildItem Env:PACKER_CACHE_DIR
 
 "Starting For loop for creating VMs"
 
-.\packer.exe build -force `
-   -var "output_dir=C:\\Users\\${windows_username}\\VirtualBox VMs\\Packer-Win2k6" `
-   -var "custom_vm_name=Packer-Win2k6" `
-   -var "hostonly_vbox_name=VirtualBox Host-Only Ethernet Adapter" `
-   ..\..\templates\win2k6\win2k6.json
+For ( $instance_count=1 ; $instance_count -le 3 ; $instance_count++) {
 
+    .\packer.exe build -force `
+       -var "output_dir=C:\\Users\\${windows_username}\\VirtualBox VMs\\Packer-win2k6-Instance-${instance_count}" `
+       -var "custom_vm_name=Packer-win2k6-Instance-${instance_count}" `
+       -var "hostonly_vbox_name=VirtualBox Host-Only Ethernet Adapter" `
+       ..\..\templates\win2k6\win2k6.json
+
+    "Done with $instance_count."
+}
