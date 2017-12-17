abacd8fa1c23e4ada66169c3cd16dac1270776ee
Danny Lai <dlai@portware.com>
2017-12-17 11:45:08 -0500

    Enabled Win2k16 for Packer

---
+++ bin/mac/win2k6.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+########################################################################
+
+echo "Setting username to be: $USER"
+
+export PACKER_CACHE_DIR="/tmp"
+echo "Setting Packer cache to /tmp"
+
+echo "Starting For loop for creating VMs"
+
+for instance_count in 1 2 3
+do
+    ./packer build -force \
+       -var "output_dir=/Users/$USER/VirtualBox VMs/Packer-win2k6-Instance-${instance_count}" \
+       -var "custom_vm_name=Packer-win2k6-Instance-${instance_count}" \
+       -var "hostonly_vbox_name=vboxnet0" \
+       ../../templates/win2k6/win2k6.json
+done
