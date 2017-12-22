506309cc48d0d12b92568dcdcd6ee883dd04953a
Danny Lai <xdanny322@gmail.com>
2017-12-21 20:07:16 -0500

    fix path problem in mac 2k6 script

--- bin/mac/win2k6.sh
+++ bin/mac/win2k6.sh
@@ -8,13 +8,13 @@ echo "Setting Packer cache to /tmp"
 
 echo "Starting For loop for creating VMs"
 
-for instance_count in 1 2 3
+for instance_count in 1 
 do
     ./packer build -force \
        -var "output_dir=/Users/$USER/VirtualBox VMs/Packer-win2k6-Instance-${instance_count}" \
        -var "custom_vm_name=Packer-win2k6-Instance-${instance_count}" \
        -var "hostonly_vbox_name=vboxnet0" \
-       -var "custom_autounattend=../../templates/win2k6/Autounattend.xml" \
-       -var "custom_winrm=../../templates/win2k6/winrm.ps1" \
+       -var "custom_autounattend=../../templates/win2k6/scripts/Autounattend.xml" \
+       -var "custom_winrm=../../templates/win2k6/scripts/winrm.ps1" \
           ../../templates/win2k6/win2k6.json
 done
